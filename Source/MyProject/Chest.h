40f3f1ebe5b2a211611e231e67eaba2981772bf2
Petros Anastasiadis <peteran20@outlook.com>
2024-03-14 21:06:44 +0100

    started work on chest

---
+++ Source/MyProject/Chest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "Chest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AChest : public AInteractable
+{
+	GENERATED_BODY()
+	
+};
